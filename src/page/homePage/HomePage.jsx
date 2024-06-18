6e5188741a66bc65110e99339db9701de292d69c
[Github <helgo1318@gmail.com>
2024-06-19 01:20:06 +0900

    feat: 불필요import 뺌

--- src/page/homePage/HomePage.jsx
+++ src/page/homePage/HomePage.jsx
@@ -1,13 +1,5 @@
-import SignInPage from '../login/SignInPage';
-import SignUpPage from '../login/SignUpPage';
-
 const HomePage = () => {
-  return (
-    <div>
-      <SignUpPage />
-      <SignInPage />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
