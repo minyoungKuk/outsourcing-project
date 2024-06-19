62933613b60dbf46640b22ffa24a9ed763bd3a15
백현명 <whitewise95@users.noreply.github.com>
2024-06-19 20:47:38 +0900

    feat: 목록페이지 완료

--- src/components/posts/SearchForm.jsx
+++ src/components/posts/SearchForm.jsx
@@ -1,13 +1,19 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 
-function SearchForm({ handleSearch, searchQuery, setSearchQuery }) {
+function SearchForm({ handleSearch, searchQuery, setSearchQuery, inputRef }) {
+
+  const defaultRef = useRef("");
+  const ref = inputRef || defaultRef;
+
   return (
     <form
       onSubmit={handleSearch}
       className="relative w-9/12 my-10 mx-auto h-14 drop-shadow-lg flex items-center"
     >
       <input
+        ref={ref}
         className="w-full h-14 bg-white rounded-3xl pl-4 pr-14 drop-shadow"
         placeholder="키워드를 입력해주세요."
         value={searchQuery}
