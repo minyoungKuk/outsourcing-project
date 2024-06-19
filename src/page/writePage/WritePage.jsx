74813beeb9a6c8eea7c43c39301a08ec0d5dae67
teoyo <bigbang723@naver.com>
2024-06-20 01:17:53 +0900

    style: checkbox 클릭 이벤트 css

--- src/page/writePage/WritePage.jsx
+++ src/page/writePage/WritePage.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import AddReview from '../../config/review.api';
-// import { Button } from '../../common/components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const TAG_LIST = [
   { id: 0, data: '#반려동물' },
@@ -13,8 +11,14 @@ const TAG_LIST = [
   { id: 6, data: '#자전거' },
   { id: 7, data: '#소풍' },
 ];
-
 function WritePage() {
+  // const [isChecked, setIsChecked] = useState(false); // 체크박스가 체크됐는지
+  const [checkedList, setCheckedList] = useState([]); // 체크된 리스트를 저장
+  // const handleCheckboxChange = () => {
+  //   // 체크박스가 체크 됐다면
+  //   setIsChecked(!isChecked);
+  //   console.log(isChecked);
+  // };
   const [review, setReview] = useState();
   useEffect(() => {
     const fetchData = async () => {
@@ -27,52 +31,23 @@ function WritePage() {
     };
     fetchData();
   }, []);
-
   const navigate = useNavigate();
-
-  const [checkedList, setCheckedList] = useState([]);
-
   const onCheckedTag = (checked, item) => {
-    if (checked && checkedList.length < 2) {
+    if (checked && checkedList.length < 4) {
       setCheckedList([...checkedList, item]);
     } else if (!checked) {
       setCheckedList(checkedList.filter((element) => element !== item));
     }
   };
   console.log(checkedList);
-
   const onSubmitBtn = async (e) => {
     e.preventDefault();
-    // 1. 게시물 추가  -> supabase로 게시물 추가하기 .select()
-    // const { data, error } = await supabase.from("posts").insert(추가할데이터).select()
-    // 아마도 data.id => post_id
-
-    // 2. POST_CATEGORY
-    // const { data, error } = await supabase
-    //   .from('POST_CATEGORY')
-    //   .insert([
-    //     { post_id: data.id, category_id1: '123123' },
-    //     {
-    //       post_id: data.id,
-    //       category_id1: '123123123123123',
-    //     },
-    //   ])
-    //   .select();
-
-    // checkedList
-    // [{ id: 1, data: '#한적한' }, { id: 2, data: '#놀이터' }]
     const toInsertData = checkedList.map((item) => {
       return {
         post_id: '123123',
         category_id1: item.id,
       };
     });
-    // toInsertData
-    // [
-    //   { post_id: '123123', category_id1: 1 },
-    //   { post_id: '123123', category_id1: 2 },
-    // ];
-    // await AddReview(newReview);
     const { data, error } = await supabase
       .from('POST_CATEGORY')
       .insert([
@@ -87,10 +62,8 @@ function WritePage() {
     if (error) {
       console.log('에러 => ', error);
     }
-
     navigate('/detail'); // 임시로 걸어놓음 (클릭 시 작성 완료한 상세페이지로 이동해야 함')
   };
-
   return (
     <div className="max-w-1080 mx-auto my-20 px-10 h-auto text-center bg-slate-100">
       <h1 className="text-3xl font-black text-gray-700 text-center mb-12">
@@ -101,13 +74,13 @@ function WritePage() {
         <div className="max-w-1080 bg-slate-400 h-96">지도 부분</div>
       </section>
       <section>
-        <div className="flex ml-12 mb-8 mt-16 content-end">
+        <div className="flex mb-8 mt-16 content-end">
           <img
             src="public\images\location-icon.png"
             alt="location-icon"
-            className="h-10"
+            className="h-10 mr-3"
           />
-          <h2 className="text-2xl font-black text-gray-700">
+          <h2 className="text-2xl font-black text-gray-700 mr-3">
             지도에서 선택한 장소이름
           </h2>
           <span className="text-sm text-slate-400 leading-10">
@@ -121,14 +94,16 @@ function WritePage() {
           <textarea
             name="content"
             placeholder="글을 입력해주세요."
-            className="w-11/12 h-80"
+            className="w-full h-60 border-2"
           />
           <div className="my-8">
             {TAG_LIST.map((item) => {
+              const isChecked = checkedList.includes(item.data);
               return (
                 <label
                   key={item.id}
-                  className="px-5 py-3 bg-white cursor-pointer rounded-full text-white bg-secondary"
+                  className={`px-5 py-3 ml-3 cursor-pointer rounded-full text-white
+                    ${isChecked ? 'bg-primary' : 'bg-secondary'}`}
                 >
                   <input
                     type="checkbox"
@@ -137,9 +112,9 @@ function WritePage() {
                     value={item.data}
                     id={item.id}
                     onChange={(e) => {
-                      onCheckedTag(e.target.checked, e.target.value);
+                      onCheckedTag(e.target.checked, item.data);
                     }}
-                    checked={checkedList.includes(item.data) ? true : false}
+                    checked={isChecked}
                   />
                   {item.data}
                 </label>
@@ -148,7 +123,7 @@ function WritePage() {
           </div>
           <button
             type="submit"
-            className="w-72 h-12 p-3 bg-sub text-primary font-black border-primary border-2"
+            className="w-72 h-12 p-3 bg-sub text-primary font-black border-primary border-2 "
             onClick={onSubmitBtn}
           >
             완료
@@ -158,5 +133,4 @@ function WritePage() {
     </div>
   );
 }
-
 export default WritePage;
